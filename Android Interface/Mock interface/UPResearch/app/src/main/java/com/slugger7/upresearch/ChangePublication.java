99f8aab84f80154f0fa1e7e2c0fec34a37ae52aa
u13027205 <duncansmallwood@yahoo.com>
2016-04-15 12:16:54 +0200

    replacing old implementation

--- Android Interface/Mock interface/UPResearch/app/src/main/java/com/slugger7/upresearch/ChangePublication.java
+++ Android Interface/Mock interface/UPResearch/app/src/main/java/com/slugger7/upresearch/ChangePublication.java
@@ -1,8 +1,11 @@
 package com.slugger7.upresearch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,6 +14,7 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,60 +24,35 @@ import java.util.List;
 /**
  * Created by Duncan on 2016/04/14.
  */
-public class ChangePublication extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ChangePublication extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_publication);
-        /*
-        *list should populate through a request from the server
-        *but for now list will be populated from strings.xml
-        */
-        Spinner pubList = (Spinner) findViewById(R.id.spnPubList);
-        assert pubList != null;
-        pubList.setOnItemSelectedListener(this);
-        ArrayAdapter dataAdapter = ArrayAdapter.createFromResource(ChangePublication.this, R.array.Publications, android.R.layout.simple_list_item_1);
-        //dropdown list layout style
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //attaching data adapter to spinner
-        pubList.setAdapter(dataAdapter);
-        //creating the meta data for the publications
 
-        //onclick event for submit
-        Button submit = (Button) findViewById(R.id.btnSubmit);
-        assert submit != null;
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Changes Submitted", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //get the components to be edited
+        //displaying the information of the publication
         EditText edtPubName = (EditText) findViewById(R.id.edtPubName);
         EditText edtPubWeb = (EditText) findViewById(R.id.edtPubWebSite);
         EditText edtDue = (EditText) findViewById(R.id.edtDueDate);
-        EditText edtProg = (EditText) findViewById(R.id.edtProgress);
+        TextView txtProg = (TextView) findViewById(R.id.txtProgress);
+        SeekBar skbProg = (SeekBar) findViewById(R.id.skbProgress);
         CheckBox cbxComplete = (CheckBox) findViewById(R.id.cbxIsCompleted);
+        CheckBox cbxAct = (CheckBox) findViewById(R.id.cbxActive);
 
         //insert the new values into the components
-        Publication curr = Globals.getPublications().get(position);
+        Publication curr = Globals.getPublications().get(Globals.getCurrentPublicationIndex());
         edtPubName.setText(curr.getName());
         edtPubWeb.setText(curr.getUrl());
         edtDue.setText(curr.getDueDate());
-        if(curr.getActive())
-            edtProg.setText(curr.getProgress());
-        else
-            edtProg.setText("Inactive");
-        cbxComplete.setChecked(curr.getComplete());
+        txtProg.setText(curr.getProgress() + "%");
+        skbProg.setProgress(curr.getProgress());
+        cbxComplete.setChecked(curr.getActive());
+        cbxAct.setChecked(curr.getActive());
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    public void onSubmit()
+    {
+        this.finish();
     }
 }
